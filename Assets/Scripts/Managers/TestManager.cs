06f41c837ec937ba8f72b27664ba7fbe5bb73b6e
YujinJeon <yujinjun@ewhain.net>
2025-08-02 21:31:18 +0900

    test: 테스톤싱글톤

---
+++ Assets/Scripts/Managers/TestManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TestManager : MonoBehaviour
+{
+    private static TestManager _instance;
+    public static TestManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject obj = new GameObject("TestManager");
+                _instance = obj.AddComponent<TestManager>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
