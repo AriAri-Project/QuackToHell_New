d32757f2c6760a77727d02af73288c0da131b8c9
YujinJeon <yujinjun@ewhain.net>
2025-08-02 18:01:17 +0900

    test: PR리뷰 테스트

---
+++ Assets/Scripts/Managers/TestManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TestManager : MonoBehaviour
+{
+    public static TestManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Keep this instance across scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Ensure only one instance exists
+        }
+    }
+
+
+}
