9fdef3a958e9b4f1605370abfd94a3941470aeb4
YujinJeon <yujinjun@ewhain.net>
2025-08-04 10:32:22 +0900

    test: pr리뷰 테스트

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int health;
+
+    public int healthPoint;
+
+    public class playerView
+    {
+    }
+
+    public class Player_model
+    {
+        public string Name;
+    }
+
+    public class PlayerPresenter
+    {
+    }
+}
